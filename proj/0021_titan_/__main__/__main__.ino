9c67a73c1bc3de10a40c7303e7f5ab78eb8afb16
Martin <martinpellizzer@gmail.com>
2021-11-30 09:30:03 +0100

    0021 initial commit

--- proj/0021_titan_/__main__/__main__.ino
+++ proj/0021_titan_/__main__/__main__.ino
@@ -1,17 +1,67 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3);
 
+#define RELAY_PIN 8
+
+int16_t set_ozone_ppb = 1500;
+
+// **********************************************************************
+// ;system
+// **********************************************************************
+typedef struct sys_t
+{
+  int8_t power_is_on;
+  int32_t countdown_timer;
+  int32_t countdown_counter;
+  int32_t countdown_millis;
+  int16_t ppb_target;
+  int16_t countdown_target;
+} sys_t;
+sys_t sys = {};
+
+// **********************************************************************
+// SENSORE
+// **********************************************************************
 typedef struct sensor_t
 {
   int16_t ppb;
 } sensor_t;
 sensor_t sensor = {};
 
-bool nextion_send_command(uint8_t arr[], uint8_t arr_size)
+enum screens
 {
-  for (uint8_t i = 0; i < arr_size; i++) Serial.write(arr[i]);
-}
+  screen_splash,
+  screen_home,
+};
+typedef struct nextion_t
+{
+  uint8_t listen_new_data_state;
+  uint8_t listen_buffer_counter;
+  uint32_t listen_current_millis;
 
+  uint8_t screen;
+  uint8_t refresh;
+
+  int8_t power_refresh;
+  int8_t ppb_refresh;
+  int8_t countdown_refresh;
+  int8_t set_ozone_ppb_refresh;
+  int8_t ppb_target_refresh;
+  int8_t countdown_target_refresh;
+} nextion_t;
+nextion_t nextion = {};
+
+const uint8_t BUFFER_SIZE = 20;
+uint8_t buffer_nextion[BUFFER_SIZE];
+uint8_t nextion_home_power[BUFFER_SIZE] = {101, 1, 3, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t nextion_home_ppb_target_sub[BUFFER_SIZE] = {101, 1, 7, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t nextion_home_ppb_target_add[BUFFER_SIZE] = {101, 1, 8, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t nextion_home_countdown_target_sub[BUFFER_SIZE] = {101, 1, 9, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t nextion_home_countdown_target_add[BUFFER_SIZE] = {101, 1, 10, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+// -----------------------------------------------------------------------------------
+// ;sensor
+// -----------------------------------------------------------------------------------
 unsigned char get_checksum(unsigned char *i, unsigned char ln)
 {
   unsigned char j, tempq = 0;
@@ -37,28 +87,281 @@ void read_sensor()
       if (tmp >= 0 && tmp <= 10000)
       {
         sensor.ppb = tmp;
-        uint8_t buff[] = {0x74, 0x30, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x2E, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
-        buff[8] = (sensor.ppb % 10000 / 1000) + 0x30;
-        buff[9] = (sensor.ppb % 1000 / 100) + 0x30;
-        buff[11] = (sensor.ppb % 100 / 10) + 0x30;
-        buff[12] = (sensor.ppb % 10 / 1) + 0x30;
-        nextion_send_command(buff, sizeof(buff));
+        nextion.ppb_refresh = 1;
       }
-      digitalWrite(8, !digitalRead(8));
     }
   }
 }
 
+// -----------------------------------------------------------------------------------
+// ;nextion
+// -----------------------------------------------------------------------------------
+bool compare_array(uint8_t *a, uint8_t *b)
+{
+  for (uint8_t i = 0; i < BUFFER_SIZE; i++) if (a[i] != b[i]) return false;
+  return true;
+}
+bool nextion_send_command(uint8_t arr[], uint8_t arr_size)
+{
+  for (uint8_t i = 0; i < arr_size; i++) Serial.write(arr[i]);
+}
+void nextion_listen()
+{
+  if (Serial.available())
+  {
+    nextion.listen_new_data_state = true;
+    buffer_nextion[nextion.listen_buffer_counter] = Serial.read();
+    if (nextion.listen_buffer_counter < BUFFER_SIZE) nextion.listen_buffer_counter++;
+    nextion.listen_current_millis = millis();
+  }
+  if (nextion.listen_new_data_state)
+  {
+    if ((millis() - nextion.listen_current_millis) > 10)
+    {
+      nextion.listen_new_data_state = false;
+      nextion.listen_buffer_counter = 0;
+      nextion_evaluate_serial();
+      for (int i = 0; i < BUFFER_SIZE; i++) buffer_nextion[i] = 0;
+    }
+  }
+}
+void nextion_evaluate_serial()
+{
+  switch (nextion.screen)
+  {
+    case screen_home:
+      if (compare_array(nextion_home_power, buffer_nextion))
+      {
+        if (sys.power_is_on)
+        {
+          sys.power_is_on = 0;
+          nextion.power_refresh = 1;
+          sys.countdown_counter = sys.countdown_timer;
+          nextion.countdown_refresh = 1;
+          digitalWrite(RELAY_PIN, LOW);
+        }
+        else
+        {
+          sys.power_is_on = 1;
+          nextion.power_refresh = 1;
+          digitalWrite(RELAY_PIN, HIGH);
+        }
+      }
+      else if (compare_array(nextion_home_ppb_target_sub, buffer_nextion))
+      {
+        sys.ppb_target -= 100;
+        if (sys.ppb_target < 0) sys.ppb_target = 0;
+        nextion.ppb_target_refresh = 1;
+      }
+      else if (compare_array(nextion_home_ppb_target_add, buffer_nextion))
+      {
+        sys.ppb_target += 100;
+        if (sys.ppb_target > 10000) sys.ppb_target = 10000;
+        nextion.ppb_target_refresh = 1;
+      }
+      else if (compare_array(nextion_home_countdown_target_sub, buffer_nextion))
+      {
+        sys.countdown_target -= 15;
+        if (sys.countdown_target < 0) sys.countdown_target = 0;
+        nextion.countdown_target_refresh = 1;
+      }
+      else if (compare_array(nextion_home_countdown_target_add, buffer_nextion))
+      {
+        sys.countdown_target += 15;
+        if (sys.countdown_target > 3 * 60) sys.countdown_target = 3 * 60;
+        nextion.countdown_target_refresh = 1;
+      }
+      break;
+  }
+}
+void nextion_replace_screen(screens screen)
+{
+  nextion.screen = screen;
+  nextion.refresh = 1;
+  uint8_t buff[] = {0x70, 0x61, 0x67, 0x65, 0x20, 0x70, 0x61, 0x67, 0x65, 0x31, 0xff, 0xff, 0xff};
+  buff[9] = screen + 0x30;
+  nextion_send_command(buff, sizeof(buff));
+}
+void nextion_manage_screen_home()
+{
+  if (nextion.power_refresh)
+  {
+    nextion.power_refresh = 0;
+    {
+      // t5.txt="Power Off"
+      uint8_t buff1[] = {0x74, 0x35, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x50, 0x6F, 0x77, 0x65, 0x72, 0x20, 0x4F, 0x66, 0x66, 0x22, 0xff, 0xff, 0xff};
+      // t5.txt="Power On"
+      uint8_t buff2[] = {0x74, 0x35, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x50, 0x6F, 0x77, 0x65, 0x72, 0x20, 0x4F, 0x6E, 0x22, 0xff, 0xff, 0xff};
+
+      if (sys.power_is_on) nextion_send_command(buff2, sizeof(buff2));
+      else nextion_send_command(buff1, sizeof(buff1));
+    }
+  }
+  if (nextion.ppb_refresh)
+  {
+    nextion.ppb_refresh = 0;
+    {
+      uint8_t buff[] = {0x74, 0x30, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x2E, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
+      buff[8] = (sensor.ppb % 100000 / 10000) + 0x30;
+      buff[9] = (sensor.ppb % 10000 / 1000) + 0x30;
+      buff[11] = (sensor.ppb % 1000 / 100) + 0x30;
+      buff[12] = (sensor.ppb % 100 / 10) + 0x30;
+      //buff[13] = (sensor.ppb % 10 / 1) + 0x30;
+      nextion_send_command(buff, sizeof(buff));
+    }
+  }
+  if (nextion.set_ozone_ppb_refresh)
+  {
+    nextion.set_ozone_ppb_refresh = 0;
+    {
+      uint8_t buff[] = {0x74, 0x31, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x2E, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
+      buff[8] = (set_ozone_ppb % 100000 / 10000) + 0x30;
+      buff[9] = (set_ozone_ppb % 10000 / 1000) + 0x30;
+      buff[11] = (set_ozone_ppb % 1000 / 100) + 0x30;
+      buff[12] = (set_ozone_ppb % 100 / 10) + 0x30;
+      //buff[13] = (set_ozone_ppb % 10 / 1) + 0x30;
+      nextion_send_command(buff, sizeof(buff));
+    }
+  }
+  if (nextion.countdown_refresh)
+  {
+    nextion.countdown_refresh = 0;
+    {
+      uint8_t buff[] = {0x74, 0x32, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
+      int8_t hrs = sys.countdown_counter / 3600;
+      int8_t mins = sys.countdown_counter % 3600 / 60;
+      int8_t secs = sys.countdown_counter % 60;
+      buff[8] = (hrs % 100 / 10) + 0x30;
+      buff[9] = (hrs % 10 / 1) + 0x30;
+      buff[11] = (mins % 100 / 10) + 0x30;
+      buff[12] = (mins % 10 / 1) + 0x30;
+      buff[14] = (secs % 100 / 10) + 0x30;
+      buff[15] = (secs % 10 / 1) + 0x30;
+      nextion_send_command(buff, sizeof(buff));
+    }
+  }
+  if (nextion.ppb_target_refresh)
+  {
+    nextion.ppb_target_refresh = 0;
+    {
+      uint8_t buff[] = {0x74, 0x33, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x2E, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
+      buff[8] = (sys.ppb_target % 100000 / 10000) + 0x30;
+      buff[9] = (sys.ppb_target % 10000 / 1000) + 0x30;
+      buff[11] = (sys.ppb_target % 1000 / 100) + 0x30;
+      buff[12] = (sys.ppb_target % 100 / 10) + 0x30;
+      nextion_send_command(buff, sizeof(buff));
+    }
+  }
+  if (nextion.countdown_target_refresh)
+  {
+    nextion.countdown_target_refresh = 0;
+    {
+      uint8_t buff[] = {0x74, 0x34, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
+      int8_t hrs = sys.countdown_target / 3600;
+      int8_t mins = sys.countdown_target / 60;
+      int8_t secs = sys.countdown_target % 60;
+      buff[8] = (hrs % 100 / 10) + 0x30;
+      buff[9] = (hrs % 10 / 1) + 0x30;
+      buff[11] = (mins % 100 / 10) + 0x30;
+      buff[12] = (mins % 10 / 1) + 0x30;
+      buff[14] = (secs % 100 / 10) + 0x30;
+      buff[15] = (secs % 10 / 1) + 0x30;
+      nextion_send_command(buff, sizeof(buff));
+    }
+  }
+}
+void nextion_manager()
+{
+  nextion_listen();
+  if (nextion.screen == screen_home) nextion_manage_screen_home();
+}
+
+
+// -----------------------------------------------------------------------------------
+// ;cycle
+// -----------------------------------------------------------------------------------
+void cycle_manager()
+{
+  countdown_run();
+  relay_manager();
+}
+
+// -----------------------------------------------------------------------------------
+// ;relay
+// -----------------------------------------------------------------------------------
+void relay_manager()
+{
+  if (sys.power_is_on)
+  {
+    if (sensor.ppb < set_ozone_ppb)
+    {
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(RELAY_PIN, LOW);
+    }
+  }
+  else
+  {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
+
+// -----------------------------------------------------------------------------------
+// ;countdown
+// -----------------------------------------------------------------------------------
+void countdown_run()
+{
+  if (sys.power_is_on)
+  {
+    if (millis() - sys.countdown_millis >= 1000)
+    {
+      sys.countdown_millis = millis();
+      sys.countdown_counter--;
+      nextion.countdown_refresh = 1;
+
+      countdown_complete();
+    }
+  }
+}
+void countdown_complete()
+{
+  if (sys.countdown_counter < 0)
+  {
+    sys.countdown_counter = sys.countdown_timer;
+    sys.power_is_on = 0;
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
+
+// -----------------------------------------------------------------------------------
+// ;core
+// -----------------------------------------------------------------------------------
 void setup()
 {
+  sys.countdown_timer = sys.countdown_counter = 10;
+
   Serial.begin(9600);
   mySerial.begin(9600);
 
-  digitalWrite(8, LOW);
-  pinMode(8, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+  pinMode(RELAY_PIN, OUTPUT);
+
+  nextion_replace_screen(screen_splash);
+  delay(2000);
+  nextion_replace_screen(screen_home);
+
+  nextion.countdown_refresh = 1;
+  nextion.set_ozone_ppb_refresh = 1;
+  nextion.ppb_target_refresh = 1;
+  nextion.countdown_target_refresh = 1;
+  nextion.power_refresh = 1;
 }
 
 void loop()
 {
+  nextion_manager();
   read_sensor();
+  cycle_manager();
 }
--- proj/scmp/__main__/__main__.ino
+++ proj/scmp/__main__/__main__.ino
@@ -9,15 +9,24 @@ typedef struct history_t
 {
   int16_t second_buff[60];
   int16_t minute_buff[60];
-  int16_t minute_buff_time[60];
-  int16_t minute_buff_date[60];
+  int16_t hour_buff[60];
+
+  int16_t hour_buff_hour[60];
+
+  int16_t minute_buff_day[60];
+  int8_t minute_buff_month[60];
+  int16_t minute_buff_year[60];
 } history_t;
 history_t history = {};
 
+#define SENSOR_COMMUNICATION_TIMEOUT 5000
 typedef struct sensor_t
 {
   int16_t ppb;
   int16_t ppb_prev;
+
+  int8_t is_communicating;
+  uint32_t is_communicating_timer;
 } sensor_t;
 sensor_t sensor1 = {};
 sensor_t sensor2 = {};
@@ -37,6 +46,12 @@ typedef struct nextion_t
   uint32_t listen_current_millis;
 
   uint8_t screen;
+  uint8_t refresh;
+  int8_t screen_history_refresh_clock;
+  int8_t screen_all_refresh_sensor_icon;
+
+  int8_t history_page;
+
 } nextion_t;
 nextion_t nextion = {};
 
@@ -62,6 +77,9 @@ const uint8_t BUFFER_SIZE = 20;
 uint8_t buffer_nextion[BUFFER_SIZE];
 uint8_t nextion_realtime_history[BUFFER_SIZE] = {101, 1, 5, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 uint8_t nextion_history_realtime[BUFFER_SIZE] = {101, 2, 1, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t nextion_history_prev[BUFFER_SIZE] = {101, 2, 10, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t nextion_history_next[BUFFER_SIZE] = {101, 2, 11, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t nextion_history_debug[BUFFER_SIZE] = {101, 2, 13, 1, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 bool compare_array(uint8_t *a, uint8_t *b)
 {
@@ -76,6 +94,7 @@ void sensor_manager()
 {
   sensor1_read();
   sensor2_read();
+  sensor1_check_communication();
 }
 
 unsigned char get_checksum(unsigned char *i, unsigned char ln)
@@ -89,6 +108,15 @@ unsigned char get_checksum(unsigned char *i, unsigned char ln)
   }
   tempq = (~tempq) + 1;
   return (tempq);
+}
+void sensor1_check_communication()
+{
+  if (millis() - is_communicating_timer >= SENSOR_COMMUNICATION_TIMEOUT)
+  {
+    is_communicating_timer = millis();
+    sensor1.is_communicating = 0;
+    nextion.screen_all_refresh_sensor_icon = 1;
+  }
 }
 void sensor1_read()
 {
@@ -104,6 +132,10 @@ void sensor1_read()
         sensor1.ppb = tmp;
         sensor1.ppb_prev = -1;
         ppb_avg = (sensor1.ppb + sensor2.ppb) / 2;
+        
+        is_communicating_timer = millis();
+        sensor1.is_communicating = 1;
+        nextion.screen_all_refresh_sensor_icon = 1;
       }
     }
   }
@@ -122,6 +154,7 @@ void sensor2_read()
         sensor2.ppb = tmp;
         sensor2.ppb_prev = -1;
         ppb_avg = (sensor1.ppb + sensor2.ppb) / 2;
+        sensor1.is_communicating = 1;
       }
     }
   }
@@ -144,9 +177,11 @@ void nextion_screen_splash_manager()
 
 void nextion_screen_realtime_manager()
 {
-  if (sensor1.ppb_prev != sensor1.ppb ||
+  if (nextion.refresh ||
+      sensor1.ppb_prev != sensor1.ppb ||
       sensor2.ppb_prev != sensor2.ppb)
   {
+    nextion.refresh = 0;
     sensor1.ppb_prev = sensor1.ppb;
     sensor2.ppb_prev = sensor2.ppb;
     {
@@ -187,41 +222,70 @@ void nextion_screen_realtime_manager()
   }
 }
 
+void nextion_screen_history_update_clock()
+{
+}
 void nextion_screen_history_manager()
 {
-  if (history_update_screen)
+  if (nextion.refresh ||
+      history_update_screen ||
+      nextion.screen_history_refresh_clock)
   {
+    nextion.refresh = 0;
     history_update_screen = 0;
     {
       /* TODO: Must change 6 to 60 */
-      for (int i = 0; i < 6; i++)
+#define NUM_PAGES 6
+      for (int i = 0; i < NUM_PAGES; i++)
       {
-        uint8_t buff[] = {0x74, 0x30, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x3F, 0x3F, 0x2E, 0x3F, 0x3F, 0x3F, 0x20, 0x50, 0x50, 0x4D, 0x20, 0x20, 0x20, 0x3F, 0x3F, 0x2D, 0x3F, 0x3F, 0x20, 0x2F, 0x20, 0x3F, 0x3F, 0x22, 0xff, 0xff, 0xff};
+        uint8_t page_index = i + (nextion.history_page * NUM_PAGES);
+        uint8_t buff[] = {0x74, 0x30, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x3F, 0x3F, 0x2E, 0x3F, 0x3F, 0x3F, 0x20, 0x50, 0x50, 0x4D, 0x20, 0x20, 0x20, 0x3F, 0x3F, 0x3A, 0x3F, 0x3F, 0x20, 0x20, 0x20, 0x3F, 0x3F, 0x2F, 0x3F, 0x2F, 0x2F, 0x3F, 0x2F, 0x3F, 0x3F, 0x22, 0xff, 0xff, 0xff};
         buff[1] = (i) + 0x30;
-        buff[8] = (history.minute_buff[i] % 100000 / 10000) + 0x30;
-        buff[9] = (history.minute_buff[i] % 10000 / 1000) + 0x30;
-        buff[11] = (history.minute_buff[i] % 1000 / 100) + 0x30;
-        buff[12] = (history.minute_buff[i] % 100 / 10) + 0x30;
-        buff[13] = (history.minute_buff[i] % 10 / 1) + 0x30;
+        buff[8] = (history.hour_buff[page_index] % 100000 / 10000) + 0x30;
+        buff[9] = (history.hour_buff[page_index] % 10000 / 1000) + 0x30;
+        buff[11] = (history.hour_buff[page_index] % 1000 / 100) + 0x30;
+        buff[12] = (history.hour_buff[page_index] % 100 / 10) + 0x30;
+        buff[13] = (history.hour_buff[page_index] % 10 / 1) + 0x30;
 
-        int prev_time = 0;
-        if (history.minute_buff_time[i] == 0) prev_time = 59;
-        else prev_time = history.minute_buff_time[i] - 1;
-        buff[21] = (prev_time % 100 / 10) + 0x30;
-        buff[22] = (prev_time % 10 / 1) + 0x30;
-        buff[24] = (history.minute_buff_time[i] % 100 / 10) + 0x30;
-        buff[25] = (history.minute_buff_time[i] % 10 / 1) + 0x30;
+        buff[21] = (history.hour_buff_hour[page_index] % 100 / 10) + 0x30;
+        buff[22] = (history.hour_buff_hour[page_index] % 10 / 1) + 0x30;
 
-        buff[29] = (history.minute_buff_date[i] % 100 / 10) + 0x30;
-        buff[30] = (history.minute_buff_date[i] % 10 / 1) + 0x30;
+        buff[24] = (0) + 0x30;
+        buff[25] = (0) + 0x30;
+
+        buff[29] = (history.minute_buff_year[page_index] % 10000 / 1000) + 0x30;
+        buff[30] = (history.minute_buff_year[page_index] % 1000 / 100) + 0x30;
+        buff[31] = (history.minute_buff_year[page_index] % 100 / 10) + 0x30;
+        buff[32] = (history.minute_buff_year[page_index] % 10 / 1) + 0x30;
+
+        buff[34] = (history.minute_buff_month[page_index] % 100 / 10) + 0x30;
+        buff[35] = (history.minute_buff_month[page_index] % 10 / 1) + 0x30;
+
+        buff[37] = (history.minute_buff_day[page_index] % 100 / 10) + 0x30;
+        buff[38] = (history.minute_buff_day[page_index] % 10 / 1) + 0x30;
+
         nextion_exec_cmd(buff, sizeof(buff));
       }
+
+      if (nextion.screen_history_refresh_clock)
       {
         uint8_t buff[] = {0x74, 0x36, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
+        //uint8_t buff[] = {0x74, 0x36, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x22, 0xff, 0xff, 0xff};
         buff[8] = (rtc.hour_curr % 100 / 10) + 0x30;
         buff[9] = (rtc.hour_curr % 10 / 1) + 0x30;
         buff[11] = (rtc.minute_curr % 100 / 10) + 0x30;
         buff[12] = (rtc.minute_curr % 10 / 1) + 0x30;
+        //buff[14] = (rtc.second_curr % 100 / 10) + 0x30;
+        //buff[15] = (rtc.second_curr % 10 / 1) + 0x30;
+        nextion_exec_cmd(buff, sizeof(buff));
+
+        nextion.screen_history_refresh_clock = 0;
+      }
+
+      {
+        uint8_t buff[] = {0x74, 0x37, 0x2E, 0x74, 0x78, 0x74, 0x3D, 0x22, 0x50, 0x61, 0x67, 0x65, 0x20, 0x31, 0x2F, 0x36, 0x22, 0xff, 0xff, 0xff};
+        buff[13] = (nextion.history_page + 1) + 0x30;
+        buff[15] = (NUM_PAGES) + 0x30;
         nextion_exec_cmd(buff, sizeof(buff));
       }
     }
@@ -264,6 +328,26 @@ void nextion_evaluate_serial()
       {
         nextion_replace_screen(screen_realtime);
       }
+      else if (compare_array(nextion_history_prev, buffer_nextion))
+      {
+        if (nextion.history_page > 0) nextion.history_page--;
+        else nextion.history_page = 0;
+        history_update_screen = 1;
+        //Serial.println(nextion.history_page);
+      }
+      else if (compare_array(nextion_history_next, buffer_nextion))
+      {
+        if (nextion.history_page < 5) nextion.history_page++;
+        else nextion.history_page = 5;
+        history_update_screen = 1;
+        //Serial.println(nextion.history_page);
+      }
+#if 0
+      else if (compare_array(nextion_history_debug, buffer_nextion))
+      {
+        rtc_ds3231.adjust(DateTime(rtc.year_curr, rtc.month_curr, rtc.day_curr, rtc.hour_curr, 59, 55));
+      }
+#endif
       break;
   }
 }
@@ -277,14 +361,15 @@ void nextion_exec_cmd(uint8_t *buff, uint8_t buff_size)
 void nextion_replace_screen(screens screen)
 {
   nextion.screen = screen;
+  nextion.refresh = 1;
   uint8_t buff[] = {0x70, 0x61, 0x67, 0x65, 0x20, 0x70, 0x61, 0x67, 0x65, 0x31, 0xff, 0xff, 0xff};
   buff[9] = screen + 0x30;
   nextion_exec_cmd(buff, sizeof(buff));
 }
 
-
-
-// rtc
+// ***********************************************************************************************
+// ;rtc
+// ***********************************************************************************************
 void rtc_manager()
 {
   now = rtc_ds3231.now();
@@ -296,10 +381,14 @@ void rtc_manager()
   rtc.second_curr = now.second();
 }
 
+// ***********************************************************************************************
+// ;history
+// ***********************************************************************************************
 void history_manager()
 {
   history_second();
   history_minute();
+  history_hour();
 }
 
 void history_second()
@@ -308,11 +397,10 @@ void history_second()
   {
     rtc.second_prev = rtc.second_curr;
 
-    for (int i = 60 - 1; i > 0; i--)
-    {
-      history.second_buff[i] = history.second_buff[i - 1];
-    }
+    for (int i = 60 - 1; i > 0; i--) history.second_buff[i] = history.second_buff[i - 1];
     history.second_buff[0] = ppb_avg;
+
+    nextion.screen_history_refresh_clock = 1;
 
 #if 1
     Serial.println(rtc.second_curr);
@@ -341,14 +429,6 @@ void history_minute()
     for (int i = 60 - 1; i > 0; i--) history.minute_buff[i] = history.minute_buff[i - 1];
     history.minute_buff[0] = avg;
 
-    for (int i = 60 - 1; i > 0; i--) history.minute_buff_time[i] = history.minute_buff_time[i - 1];
-    history.minute_buff_time[0] = rtc.minute_curr;
-
-    for (int i = 60 - 1; i > 0; i--) history.minute_buff_date[i] = history.minute_buff_date[i - 1];
-    history.minute_buff_date[0] = rtc.day_curr;
-
-    history_update_screen = 1;
-
 #if 1
     for (int i = 0; i < 60; i++)
     {
@@ -357,10 +437,52 @@ void history_minute()
     }
     Serial.println();
 #endif
-
   }
 }
 
+void history_hour()
+{
+  if (rtc.hour_prev != rtc.hour_curr)
+  {
+    rtc.hour_prev = rtc.hour_curr;
+
+    int32_t avg = 0;
+    for (int i = 0; i < 60; i++) avg += history.minute_buff[i];
+    avg /= 60;
+
+    for (int i = 60 - 1; i > 0; i--) history.hour_buff[i] = history.hour_buff[i - 1];
+    history.hour_buff[0] = avg;
+
+    for (int i = 60 - 1; i > 0; i--) history.hour_buff_hour[i] = history.hour_buff_hour[i - 1];
+    history.hour_buff_hour[0] = rtc.hour_curr;
+
+    for (int i = 60 - 1; i > 0; i--) history.minute_buff_day[i] = history.minute_buff_day[i - 1];
+    history.minute_buff_day[0] = rtc.day_curr;
+
+    for (int i = 60 - 1; i > 0; i--) history.minute_buff_month[i] = history.minute_buff_month[i - 1];
+    history.minute_buff_month[0] = rtc.month_curr;
+
+    for (int i = 60 - 1; i > 0; i--) history.minute_buff_year[i] = history.minute_buff_year[i - 1];
+    history.minute_buff_year[0] = rtc.year_curr;
+
+    history_update_screen = 1;
+
+#if 1
+    Serial.println("**************************************************************");
+    for (int i = 0; i < 60; i++)
+    {
+      Serial.print(history.hour_buff_hour[i]);
+      Serial.print(",");
+    }
+    Serial.println();
+    Serial.println("**************************************************************");
+#endif
+  }
+}
+
+// ***********************************************************************************************
+// ;core
+// ***********************************************************************************************
 void setup()
 {
   Serial.begin(9600);
@@ -370,7 +492,12 @@ void setup()
 
   if (!rtc_ds3231.begin()) {}
   else {}
-  if (rtc_ds3231.lostPower()) rtc_ds3231.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  if (rtc_ds3231.lostPower())
+  {
+    rtc_ds3231.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  //rtc_ds3231.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //rtc_ds3231.adjust(DateTime(2014, 1, 21, 8, 59, 50));
   now = rtc_ds3231.now();
   rtc.year_curr = rtc.year_prev = now.year();
   rtc.month_curr = rtc.month_prev = now.month();
